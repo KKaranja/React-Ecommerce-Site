82d1859d5ac9f0a97f1196eedcc95e50910e8a7f
Isaac Kimura <Isaac.Kimura@iprospect.com>
2021-09-20 10:44:58 +0300

    seperated my project (client & server),installed required dependancies for the back end, Done with register,login,get user,get all users, create product ,get new products, get product by category, get all products endpoins

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connected successfully"))
+  .catch((err) => {
+    console.log(err);
+  });
+app.use(express.json());
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/products", productRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running on port 5000");
+});
